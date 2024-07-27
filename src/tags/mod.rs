bf1109f6bcab5fb02bad1955e81287f006ed4b95
HenrySteinmetz <hsteinm2@lspb.de>
2024-07-27 22:57:39 +0200

    added the first file system elements to the tag system

--- src/tags/mod.rs
+++ src/tags/mod.rs
@@ -1,6 +1,14 @@
-use std::{fmt::Display, ops::Deref, path::PathBuf};
+use std::{
+    fmt::Display,
+    ops::Deref,
+    path::{Path, PathBuf},
+};
 
 use convert_case::{Case, Casing};
+
+pub mod fs;
+
+use fs::*;
 
 #[derive(Debug, Clone)]
 pub struct Tag {
@@ -36,6 +44,22 @@ impl Display for TagID {
     }
 }
 
+pub struct InvalidFileName;
+
+impl TryFrom<&Path> for TagID {
+    type Error = InvalidFileName;
+
+    fn try_from(value: &Path) -> Result<Self, Self::Error> {
+        Ok(TagID(
+            value
+                .file_stem()
+                .and_then(|osstr| osstr.to_str())
+                .ok_or(InvalidFileName)?
+                .to_string(),
+        ))
+    }
+}
+
 impl TagID {
     pub fn new(value: &str) -> Self {
         TagID(value.to_string())
@@ -43,5 +67,9 @@ impl TagID {
 
     pub fn parse<T: AsRef<str>>(value: T) -> Self {
         TagID(value.as_ref().to_case(Case::Snake))
+    }
+
+    pub fn get_path(&self) -> PathBuf {
+        get_save_dir().join(format!("{}.json", self))
     }
 }
